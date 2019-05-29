e13b7086474a1ce705aaca3913a0fb0eee9af854
Niks-Niks <44062180+Niks-Niks@users.noreply.github.com>
2019-05-29 22:28:18 +0200

    Update init_db.sql

--- src/com/startjava/lesson_5/init_db.sql
+++ src/com/startjava/lesson_5/init_db.sql
@@ -1,12 +1,12 @@
 ﻿INSERT INTO jaeger(id, modelName, mark, height, weight, status, origin, launch, kaijukill)
-	VALUES
-	       	('Gipsy Avenger' ,	'Mark-6' , '268' , '2004' ,' destroyed' , 'Sydney' ,	      '2034.01.12' , '1'),
-		('Bracer Phoenix' ,	'Mark-5' , '232' , '2128' , 'destroyed' , 'China' ,	      '2025.11.01' , '1'),
-		(' Brawler Yukon' ,   	'Mark-1' , '260' , '3412' , 'destroyed' , 'USA' ,	      '2015.01.30' , '2'),
-		(' November Ajax' ,  	'Mark-6' , '259' , '2134' , 'destroyed' , 'Santa Monica' ,    '2011.01.30' , '1'),
-		(' Obsidian Fury' ,	'Mark-1' , '211' , '1890' , 'destroyed' , 'Newton Geiszler' , '2035.03.30' , '0'),
-		(' Romeo Blue' ,	'Mark-1' , '255' , '7775' , 'destroyed' , 'USA' , 	      '2015.12.15' , '2'),
-		(' Saber Athena' ,     	'Mark-7' , '252' , '1628' , 'destroyed' , 'USA' , 	      '2033.12.20' , '1'),
-		(' Victory Alpha' ,     'Mark-1' , '246' , '2128' , 'destroyed' , 'Japan' , 	      '2016.06.05' , '1'),
-		(' Titan Redeemer' ,    'Mark-6' , '246' , '2328' , 'destroyed' , 'USA' , 	      '2022.01.25' , '1'),
-		(' Tacit Ronin' ,       'Mark-1' , '244' , '7450' , 'destroyed' , 'Japan' , 	      '2015.12.06' , '3');
+     VALUES
+	  ('Gipsy Avenger',  'Mark-6', '268', '2004',' destroyed', 'Sydney',	       '2034.01.12', '1'),
+	  ('Bracer Phoenix', 'Mark-5', '232', '2128', 'destroyed', 'China',	       '2025.11.01', '1'),
+	  ('Brawler Yukon',  'Mark-1', '260', '3412', 'destroyed', 'USA',	       '2015.01.30', '2'),
+	  ('November Ajax',  'Mark-6', '259', '2134', 'destroyed', 'Santa Monica',     '2011.01.30', '1'),
+	  ('Obsidian Fury',  'Mark-1', '211', '1890', 'destroyed', 'Newton Geiszler',  '2035.03.30', '0'),
+          ('Romeo Blue',     'Mark-1', '255', '7775', 'destroyed', 'USA', 	       '2015.12.15', '2'),
+	  ('Saber Athena',   'Mark-7', '252', '1628', 'destroyed', 'USA', 	       '2033.12.20', '1'),
+	  ('Victory Alpha',  'Mark-1', '246', '2128', 'destroyed', 'Japan', 	       '2016.06.05', '1'),
+	  ('Titan Redeemer', 'Mark-6', '246', '2328', 'destroyed', 'USA', 	       '2022.01.25', '1'),
+	  ('Tacit Ronin',    'Mark-1', '244', '7450', 'destroyed', 'Japan', 	       '2015.12.06', '3');
