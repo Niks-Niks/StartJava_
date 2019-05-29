315785eba4a3cc7482d917cd288a958b3acd01d1
Niks-Niks <44062180+Niks-Niks@users.noreply.github.com>
2019-05-29 18:08:55 +0200

    Update querise.sql

--- src/com/startjava/lesson_5/querise.sql
+++ src/com/startjava/lesson_5/querise.sql
@@ -1,6 +1,6 @@
 ﻿SELECT * FROM jaeger;
 SELECT * FROM jaeger WHERE status != 'destroyed';
-SELECT * FROM jaeger WHERE mark = 'Mark-1' AND mark = 'Mark-6';
+SELECT * FROM jaeger WHERE mark IN ('Mark-1', 'Mark-6');
 SELECT * FROM jaeger ORDER BY mark DESC; 
 SELECT * FROM jaeger WHERE launch = (SELECT MAX(launch) FROM jaeger);
 SELECT * FROM jaeger WHERE kaijuKill = (SELECT MAX(kaijuKill) FROM jaeger);
