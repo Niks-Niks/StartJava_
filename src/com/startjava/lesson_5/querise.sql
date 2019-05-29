47be880e613290be5025e07e8d1eef6de6f14c9c
Niks-Niks <44062180+Niks-Niks@users.noreply.github.com>
2019-05-29 13:47:50 +0200

    Add files via upload

---
+++ src/com/startjava/lesson_5/querise.sql
@@ -0,0 +1,9 @@
+﻿select * from jaeger;
+select * from jaeger where status != 'destroyed';
+select * from jaeger where mark = 'Mark-1' and mark = 'Mark-6';
+select * from jaeger order by mark desc; 
+select * from jaeger where launch = (select MAX(launch) from jaeger);
+select * from jaeger where kaijuKill = (select MAX(kaijuKill) from jaeger); /  select * from jaeger where kaijuKill = (select MIN(kaijuKill) from jaeger);
+select AVG(weight) from jaeger;
+update jaeger set kaijukill = kaijukill + 1 where status != 'destroyed';
+delete from jaeger where status = 'destroyed';
